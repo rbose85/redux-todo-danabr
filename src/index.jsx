8620f6a9c70bf0f73574073d15101e6129201107
Rajiv Bose <rbose85@users.noreply.github.com>
2016-03-05 10:02:13 +0000

    move reducers into their own file

--- src/index.jsx
+++ src/index.jsx
@@ -3,59 +3,15 @@ import './styles.css'
 import React, { Component, PropTypes } from 'react'
 import ReactDOM from 'react-dom'
 import { combineReducers, createStore } from 'redux'
-
-
-// reducers
-
-const todo = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      }
-    case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state
-      }
-
-      return {
-        ...state,
-        completed: !state.completed
-      }
-    default:
-      return state
-  }
-}
-
-export const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ]
-    case 'TOGGLE_TODO':
-      return state.map((t) => todo(t, action))
-    default:
-      return state
-  }
-}
-
-export const visibilityFilter = (state = 'SHOW_ALL', action) => {
-  switch (action.type) {
-    case 'SET_VISIBILITY_FILTER':
-      return action.filter
-    default:
-      return state
-  }
-}
+import * as reducers from './reducers'
 
 
 // store
 
-const todoApp = combineReducers({ todos, visibilityFilter })
+const todoApp = combineReducers({
+  todos: reducers.todos,
+  visibilityFilter: reducers.visibilityFilter
+})
 const store = createStore(todoApp)
 
 
